373ada7585f22db61083d069430fff69d100d62d
Zalman Zoteev <sz.zoteev@gmail.com>
2023-07-23 22:48:25 +0300

    Initial Commit

---
+++ ColorView/ColorViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorViewApp.swift
+//  ColorView
+//
+//  Created by Zalman Zoteev on 23/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
